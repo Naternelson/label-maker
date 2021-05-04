da90a71c07f64aacea4e4badb24bcb07c354ffa8
Nate <7nater.nelson@gmail.com>
2021-05-03 19:53:39 -0600

    Validate Presence of Product Name

--- backend/app/models/product.rb
+++ backend/app/models/product.rb
@@ -2,6 +2,8 @@ class Product < ApplicationRecord
     has_many :product_item_parameters
     has_many :item_code_parameters, through: :product_item_parameters
     has_many :items, dependent: :destroy
+    
+    validates :name, presence: :true
 
     def has_item_params?
         item_code_parameters.length > 0
